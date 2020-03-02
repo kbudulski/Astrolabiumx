6b0e18833e62042f164e2210a321a262227464e5
Cassivellaunus <Cassivellaunus@users.noreply.github.com>
2020-03-02 17:04:44 +0100

    before cli update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { RootComponent } from './root/root.component';
+import { HeaderComponent } from './header/header.component';
+import { SpaceGalleryComponent } from './space-gallery/space-gallery.component';
+import { MarsWeatherComponent } from './mars-weather/mars-weather.component';
+import { SpacexComponent } from './spacex/spacex.component';
+import { PicOfTheDayComponent } from './space-gallery/pic-of-the-day/pic-of-the-day.component';
+import { ImageVideoLibraryComponent } from './space-gallery/image-video-library/image-video-library.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RootComponent,
+    HeaderComponent,
+    SpaceGalleryComponent,
+    MarsWeatherComponent,
+    SpacexComponent,
+    PicOfTheDayComponent,
+    ImageVideoLibraryComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
